edc16c999e18d8db037b079ed01771f194c79fac
Faisal0001 <ansarifaisal0001@gmail.com>
2020-12-16 13:36:11 +0545

    initail commit

---
+++ accounts/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class UserAdmin(UserAdmin):
+	list_display = ('email', 'username', 'date_joined', 'last_login', 'is_superuser', 'is_active', 'is_admin', 'is_staff')
+	search_fields =  ('email', 'username', 'date_joined', 'last_login', 'is_superuser', 'is_active', 'is_admin', 'is_staff')
+	readonly_fields = ('date_joined', 'last_login',)
+	filter_horizontal = ()
+	list_filter = ()
+	ordering = ('email',)
+	fieldsets = ()
+
+admin.site.register(User, UserAdmin)
